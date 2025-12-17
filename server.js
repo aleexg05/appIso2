5accfc4369f3d913f4f32c3a8c4b25e98cf75f60
aleexg05 <alexhey744@gmail.com>
2025-12-17 09:38:09 +0100

    git commit

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ const db = new Low(adapter, { students: [] }); // Valor per defecte
 
 // Middleware
 app.use(express.json());
+
+// CORS headers para permitir peticiones desde cualquier origen
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Serveix fitxers estàtics des de la carpeta 'public'
 app.use(express.static(join(__dirname, 'public'))); 
 
